3f7569963b186821ae349cbdda745e81fc7d62ae
Niclas Stoffregen <niclas.stoffregen@gmx.de>
2020-06-19 16:33:12 +0200

    added additional cl arguments

--- ublEnroll.py
+++ ublEnroll.py
@@ -8,6 +8,8 @@ readernr = sys.argv[1]
 passwd = sys.argv[2]
 begin = sys.argv[3]
 end = sys.argv[4]
+bib = sys.argv[5]
+seatingArea = sys.argv[6]
 def main():
         print("---")
         print("starting")
@@ -18,26 +20,27 @@ def main():
         #print(login.text)
         if(token == None and "Achtung, zuviele Nutzer sind gerade angemeldet." in login.json()['msg']):
                 print("two many people on site")
-                sys.exit(0)
+                
         elif(token == None):
                 print("something went wrong, token null")
-                sys.exit(0)
-        dataSeat = {'institution':'Bibliotheca Albertina',
-                                'area':'Mitte 3. OG',
-                                'from_date':str(date.today() + timedelta(days=2)),
-                                'from_time':begin,
-                                'until_time':end,
-                                'readernumber':readernr,
-                                'token':token}
-        #print(dataSeat)
-        booking = requests.post('https://seats.ub.uni-leipzig.de/api/booking/booking',data = dataSeat)
-        #print(booking.json())
-        if(booking.json()['message'] != "outofreach" and booking.json()['bookingCode'] != ""):
-                print("booking sucessfully")
-        elif(booking.json()['bookingCode'] == ""):
-                print("all seats occupied")
+                
         else:
-                print("booking not sucessfull")
+                dataSeat = {'institution':bib,
+                                        'area':seatingArea,
+                                        'from_date':str(date.today() + timedelta(days=2)),
+                                        'from_time':begin,
+                                        'until_time':end,
+                                        'readernumber':readernr,
+                                        'token':token}
+                #print(dataSeat)
+                booking = requests.post('https://seats.ub.uni-leipzig.de/api/booking/booking',data = dataSeat)
+                #print(booking.json())
+                if(booking.json()['message'] != "outofreach" and booking.json()['bookingCode'] != ""):
+                        print("booking sucessfully")
+                elif(booking.json()['bookingCode'] == ""):
+                        print("all seats occupied")
+                else:
+                        print("booking not sucessfull")
         print("terminating")
         print(datetime.now())
 
