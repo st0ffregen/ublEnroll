6a41f23f308b9f78346e5a5c5cf6d5f7dec9a7d7
Niclas Stoffregen <niclas.stoffregen@gmx.de>
2020-06-22 13:06:26 +0200

    added fallback option and restructured

--- ublEnroll.py
+++ ublEnroll.py
@@ -4,46 +4,83 @@ import requests
 from datetime import timedelta, date, datetime
 import sys
 
-readernr = sys.argv[1]
-passwd = sys.argv[2]
-begin = sys.argv[3]
-end = sys.argv[4]
-bib = sys.argv[5]
-seatingArea = sys.argv[6]
+
+
+def login(cardnumber, password):
+        dataLogin = {'readernumber':cardnumber,'password':password}
+        login = requests.post('https://seats.ub.uni-leipzig.de/api/booking/login', data = dataLogin)
+        token = login.json()['token']
+
+        if(token == "null" and "Achtung, zuviele Nutzer sind gerade angemeldet." in login.json()['msg']):
+                print("two many people on site")
+                print(login.text)
+                return 1
+        elif(token == "null"):
+                print("something went wrong, token null")
+                print(login.text)
+                return 1
+        else:
+                return token
+      
 def main():
+
+        #processing cli arguments
+        readernr = sys.argv[1]
+        passwd = sys.argv[2]
+        begin = sys.argv[3]
+        end = sys.argv[4]
+        bib = sys.argv[5]
+        seatingArea = sys.argv[6]
+        seatingAreaFallback  = sys.argv[7]
+        days = sys.argv[8]
+
         print("---")
         print("starting")
         print(datetime.now())
-        dataLogin = {'readernumber':readernr,'password':passwd}
-        login = requests.post('https://seats.ub.uni-leipzig.de/api/booking/login', data = dataLogin)
-        token = login.json()['token']
-        #print(login.text)
-        if(token == None and "Achtung, zuviele Nutzer sind gerade angemeldet." in login.json()['msg']):
-                print("two many people on site")
-                
-        elif(token == None):
-                print("something went wrong, token null")
-                
+        token = login(readernr, passwd)
+        
+        if(token != 1):
+                return bookSeat(readernr, begin,end,bib,seatingArea,seatingAreaFallback, days, token, False)
         else:
-                dataSeat = {'institution':bib,
-                                        'area':seatingArea,
-                                        'from_date':str(date.today() + timedelta(days=7)),
-                                        'from_time':begin,
-                                        'until_time':end,
-                                        'readernumber':readernr,
-                                        'token':token}
-                #print(dataSeat)
-                booking = requests.post('https://seats.ub.uni-leipzig.de/api/booking/booking',data = dataSeat)
-                #print(booking.json())
-                if(booking.json()['message'] != "outofreach" and booking.json()['bookingCode'] != ""):
-                        print("booking sucessfully")
-                elif(booking.json()['message'] == "outofreach"):
-                        print("time or date is not valid")
-                elif(booking.json()['bookingCode'] == ""):
-                        print("all seats occupied")
-                else:
-                        print("booking not sucessfull")
-        print("terminating")
-        print(datetime.now())
+                print("terminating")
+                print(datetime.now())
+                return 1
+
+def bookSeat(cardnumber, begin, end, bib, seatingArea, seatingAreaFallback, days, token, fallback):
+        
+        print("sending query")
+
+        dataSeat = {'institution':bib,
+                'area':seatingArea,
+                'from_date':str(date.today() + timedelta(days=int(days))),
+                'from_time':begin,
+                'until_time':end,
+                'readernumber':cardnumber,
+                'token':token
+                }
+        
+        booking = requests.post('https://seats.ub.uni-leipzig.de/api/booking/booking',data = dataSeat)
+       
+        if(booking.json()['message'] != "outofreach" and booking.json()['bookingCode'] != ""):
+                print("booking sucessfully")
+                print("terminating")
+                print(datetime.now())
+                return 0
+        elif(booking.json()['message'] == "outofreach"):
+                print(booking.text)
+                print("time or date is not valid")
+                print("terminating")
+                print(datetime.now())
+                return 1
+        elif(booking.json()['bookingCode'] == "" and fallback == False):
+                print("all seats occupied")
+                print("try second seating area")
+                return bookSeat(cardnumber, begin, end, bib, seatingAreaFallback, seatingAreaFallback, days, token, True)
+        else:
+                print("booking not sucessfull")
+                print("terminating")
+                print(datetime.now())
+                return 1
+        
 
 main()
